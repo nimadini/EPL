4931b7b7e8975c06c7a4129fc9f4a35c83331fbe
Nima Dini <nima.dini@utexas.edu>
2016-01-31 23:33:27 -0600

    undone 1st approach

--- Project1a/Vector.h
+++ Project1a/Vector.h
@@ -16,33 +16,87 @@ const int default_size = 8;
 template <typename T>
 class vector {
 private:
-	T* data;
-	uint64_t begin;
-	uint64_t end;
-	uint64_t capacity;
+	T* array_front;
+	T* array_back;
+
+	uint64_t fidx;
+	uint64_t bidx;
+	
+	uint64_t fcap;
+	uint64_t bcap;
 
 	void init(void) {
-		this->data=nullptr;
-		this->begin=end=0;
-		this->capacity=default_size;
+		this->array_front=nullptr;
+		this->array_back=nullptr;
+		this->fidx=this->bidx=0;
+		this->fcap=this->bcap=default_size;
 	}
 
 	void copy(vector const& that) {
-		this->begin=that.begin;
-		this->end=that.end;
-		this->capacity=that.capacity;
-		this->data=new T[capacity];
+		this->fidx=that.fidx;
+		this->bidx=that.bidx;
 
-		for (uint64_t i=0; i<capacity; i++) {
-			this->data[i]=that.data[i];
+		this->fcap=that.fcap;
+		this->bcap=that.bcap;
+
+		this->array_front=new T[fcap];
+		this->array_back=new T[bcap];
+
+		for (uint64_t i=0; i<fidx; i++) {
+			this->array_front[i]=that.array_front[i];
+		}
+
+		for (uint64_t i=0; i<bidx; i++) {
+			this->array_back[i]=that.array_back[i];
 		}
 	}
 
 	void destroy(void) {
+		destroy_front();
+		destroy_back();
+	}
+
+	void destroy_front(void) {
 		// Field data might be nullptr.
-		if (this->data) {
-			delete[] this->data;
+
+		if (this->array_front) {
+			delete[] this->array_front;
 		}
+	}
+
+	void destroy_back(void) {
+		// Field data might be nullptr.
+
+		if (this->array_back) {
+			delete[] this->array_back;
+		}
+	}
+
+	void allocIfNull(void) {
+		// Check if data has not yet been initialized (nullptr).
+		if (!this->array_front) {
+			this->array_front=new T[fcap];
+		}
+
+		if (!this->array_back) {
+			this->array_back=new T[bcap];
+		}
+	}
+
+	uint64_t remaining_back(void) {
+		return bcap-bidx-1;
+	}
+
+	uint64_t remaining_front(void) {
+		return fcap-fidx-1;
+	}
+
+	uint64_t size_back(void) const {
+		return bidx;
+	}
+
+	uint64_t size_front(void) const {
+		return fidx;
 	}
 
 public:
@@ -61,9 +115,10 @@ public:
 		if (!size) {
 			init();
 		} else {
-			this->data=new T[size];
-			this->capacity=size;
-			this->begin=end=0;
+			this->array_front=new T[size+1];
+			this->array_back=new T[size+1];
+			this->fidx=this->bidx=0;
+			this->fcap=this->bcap=size+1;
 		}
 	}
 
@@ -75,14 +130,14 @@ public:
 	vector& operator=(vector const& rhs) {
 		if (this != &rhs) {
 			destroy();
-			copty(rhs);
+			copy(rhs);
 		}
 
 		return *this;
 	}
 
 	uint64_t size(void) const {
-		return end-begin;
+		return size_back() + size_front();
 	}
 
 	T& operator[](uint64_t k) {
@@ -90,51 +145,107 @@ public:
 			throw std::out_of_range("subscript out of range");
 		}
 
-		return data[begin+k+1];
+		uint64_t idx=fidx;
+
+		while (idx > 1 && k != 0) {
+			idx -= 1;
+			k -= 1;
+		}
+
+		if (k == 0) {
+			return array_front[idx];
+		} 
+
+		if (fidx) {
+			k-=1;
+		}
+
+		idx=1;
+
+		while (idx <= bidx && k != 0) {
+			idx+=1;
+			k -= 1;
+		}
+
+		return array_back[idx];
 	}
 
-	const T& operator[](uint64_t k) const {
+
+
+	/* const T& operator[](uint64_t k) const {
 		if (k >= size()) {
 			throw std::out_of_range("subscript out of range");			
 		}
 
-		return data[begin+k+1];
-	}
+		return data[begin+k];
+	} */
 
 	void push_back(const T& elem) {
-		// Check if data has not yet been initialized (nullptr).
-		if (!this->data) {
-			this->data=new T[capacity];
-		}
+		allocIfNull();
 
-		// Check if data[] is large enough to fit the new element.
-		if (end+1 == capacity) {
-			capacity*=2; // Double the capacity.
+		// If there is no empty spot at the back of the array.
+		if (!remaining_back()) {
+			bcap*=2; // Double the capacity.
 
-			T* newData=new T[capacity];
+			T* array_back_new=new T[bcap];
 
-			for (uint64_t i=begin; i < end; i++) {
-				newData[i+1]=this->data[i+1];
+			for (uint64_t i=0; i <= bidx; i++) {
+				array_back_new[i]=this->array_back[i];
 			}
 
-			destroy();
-			this->data=newData;
+			destroy_back();
+			this->array_back=array_back_new;
 		}
 
-		T newElem{elem}; // Calling T's copy constructor.
-		this->data[++end]=newElem;
+		//T newElem{elem}; // Calling T's copy constructor???
+		this->array_back[++bidx]=T{elem};
 	}
 
-	void push_front(const T&) {
-		if (!begin) {
+	void pop_back(void) {
+		// If there is no empty spot at the back of the array.
 
+		if (bidx != 0) {
+			this->array_back[bidx--].~T();
+		} else if (fidx != 0)
+
+		if (bidx == 0) {
+			throw std::out_of_range("subscript out of range");
 		}
+
+
+		
 	}
+
+	void push_front(const T& elem) {
+		allocIfNull();
+		
+		if (!remaining_front()) {
+			fcap*=2;
+
+			T* array_front_new=new T[fcap];
+
+			for (uint64_t i=0; i <= fidx; i++) {
+				array_front_new[i]=this->array_front[i];
+			}
+
+			destroy_front();
+			this->array_front=array_front_new;
+		}
+
+		this->array_front[++fidx]=T{elem};
+	}
+
 
 	void print(void) {
-		for (uint64_t i=begin; i<end; i++) {
-			cout << data[i+1] << " ";
+		for (uint64_t i=fidx; i != 0; i-=1) {
+			cout << array_front[i] << " ";
 		}
+
+		for (uint64_t i=1; i<=bidx; i+=1) {
+			cout << array_back[i] << " ";
+		}
+
+		cout << "\n";
 	}
 };
 
--- Project1a/main.cpp
+++ Project1a/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <vector>
+
 #include "Vector.h"
 
 using std::cout;
@@ -10,11 +12,34 @@ int main(void) {
 
 	vector<int> v;
 
-	for (int i=0; i < 91; i++) {
+	std::vector<int> std_v;
+
+	for (int i=0; i < 9; i++) {
+		std_v.push_back(i);
+		// v.print();
+		std_v.insert(std_v.begin(), i);
+
+		// v.push_back(i);
 		v.push_back(i);
+	//	v.push_front(i);
+		// v.push_front(i);
 	}
 
 	v.print();
+
+	for (int i = 0; i < 4; i++) {
+		v.pop_back();
+	}
+
+	v.print();
+
+	// for (int i=0; i<std_v.size(); i++) {
+	// 	cout << std_v[i] << " ";
+	// }
+
+	// cout << v[0] << v[1] << v[2];
+
+	// cout << "\n";
 
 	return 0;
 }
